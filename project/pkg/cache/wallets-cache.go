4d8ad240dd76111ba40e73e01c5493d6a5ca4c22
ergossteam <ergossteam@gmail.com>
2021-11-26 23:52:48 +0600

    feat: project deleted

--- project/pkg/cache/wallets-cache.go
+++
@@ -1,9 +0,0 @@
-package cache
-
-import "example.com/internal/store/psql/models"
-
-type WalletsCache interface {
-	Set(key string, value *models.Wallets)
-	Get(key string) *models.Wallets
-	Delete(key string)
-}
